9637971c30317efefc68cdb19a6f7561b8feb2d2
arulhaerul-glitch <arulhaerul@gmail.com>
2026-03-03 09:17:27 +0700

    Create App component for React login and dashboard UI

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Login} />
+        <Route path='/dashboard' component={Dashboard} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
